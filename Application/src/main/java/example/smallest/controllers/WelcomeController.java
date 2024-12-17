803738d4ff7f1f427f0b994a1e7eb68aecd2d624
Kadarlasanthosh <160042871+Kadarlasanthosh@users.noreply.github.com>
2024-12-17 20:58:22 +0530

    Update WelcomeController.java

--- Application/src/main/java/example/smallest/controllers/WelcomeController.java
+++ Application/src/main/java/example/smallest/controllers/WelcomeController.java
@@ -16,6 +16,6 @@ public class WelcomeController {
 		
 		
 		
-		return "Congratulations DevOps Engineers!!! AWS DevOps is a recesion-proof IT Career, All the best for your Bright future"; //"application/json" mean this is a text not a redirect
+		return "Automating a CICD project using Jenkins pipeline, Automating a provisioning AWS resources using Terraform, Deploying them using Ansible in  Kubernetes CLuster "; //"application/json" mean this is a text not a redirect
 	}
 }
